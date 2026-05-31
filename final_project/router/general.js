127f32dfea49214f746f06d67c603c24568dfe42
nam <fatima.basim.shmmri@gmail.com>
2026-05-31 04:09:07 +0300

    axios add

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,7 +4,7 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
+const axios = require('axios');
 public_users.post("/register", async (req,res) => {
   const {username, password} = req.body;
   try {
